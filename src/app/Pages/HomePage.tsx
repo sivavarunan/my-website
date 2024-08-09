25dae4fdeec48dc5f1b837d708ff22aeb40357a6
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-09 21:42:01 +0530

    text genertion effect added

--- src/app/Pages/HomePage.tsx
+++ src/app/Pages/HomePage.tsx
@@ -1,18 +1,43 @@
+"use client"
+import React from 'react';
 import { NavbarDemo } from '@/Components/Navbar';
+import { Vortex } from '@/Components/vortex';
+import { TextGenerateEffect } from "@/Components/text-generate-effect";
+
+const words = `Welcome to My Web Page`
+export function TextGenerate() {
+  return <TextGenerateEffect duration={2} filter={false} words={words} className='' />;
+}
 
 const HomePage: React.FC = () => {
   return (
-    <main className=" min-h-screen flex flex-col items-center justify-center">
+    <main className="min-h-screen flex flex-col items-center justify-center">
       <NavbarDemo />
+
+      <div className="w-[calc(100%-4rem)] mx-auto rounded-md h-[30rem] overflow-hidden mt-20">
+        <Vortex
+          backgroundColor="#000000"
+          className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+          baseHue={180}
+          baseRadius={0.5}
+          baseSpeed={-1}
+          rangeY={120}
+          particleCount={500}
       
-      <div className="shadow-md rounded-lg p-8 w-11/12 md:w-2/3 lg:w-1/2 text-center mt-20">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
-        <p className="text-gray-700 mb-4">
-          We are a team of dedicated professionals providing top-notch services to our clients. Our mission is to deliver high-quality products and services that exceed our customers' expectations.
-        </p>
-        <p className="text-gray-700 mb-4">
-          Explore our services and products to see how we can help you achieve your goals.
-        </p>
+        >
+          <h1 className="text-red text-2xl md:text-6xl font-bold text-center mb-4">
+            <TextGenerate/>
+          </h1>
+          <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+            Feel Free to Explore About Me..
+            </p>
+          <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+            and Let Me Know..
+          </p>
+          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mt-6">
+            Hay
+          </button>
+        </Vortex>
       </div>
     </main> 
   );
