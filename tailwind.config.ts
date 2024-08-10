29f837aee1279fea3905871b289c183ca0a280bd
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-10 11:57:07 +0530

    addded a background gradient component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,11 @@ module.exports = {
       animation: {
         shimmer: "shimmer 2s linear infinite",
         move: "move 5s linear infinite",
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
       },
       keyframes: {
         shimmer: {
@@ -30,6 +35,39 @@ module.exports = {
           move: {
             "0%": { transform: "translateX(-200px)" },
             "100%": { transform: "translateX(200px)" },
+          },
+          moveHorizontal: {
+            "0%": {
+              transform: "translateX(-50%) translateY(-10%)",
+            },
+            "50%": {
+              transform: "translateX(50%) translateY(10%)",
+            },
+            "100%": {
+              transform: "translateX(-50%) translateY(-10%)",
+            },
+          },
+          moveInCircle: {
+            "0%": {
+              transform: "rotate(0deg)",
+            },
+            "50%": {
+              transform: "rotate(180deg)",
+            },
+            "100%": {
+              transform: "rotate(360deg)",
+            },
+          },
+          moveVertical: {
+            "0%": {
+              transform: "translateY(-50%)",
+            },
+            "50%": {
+              transform: "translateY(50%)",
+            },
+            "100%": {
+              transform: "translateY(-50%)",
+            },
           },
         },
       },
