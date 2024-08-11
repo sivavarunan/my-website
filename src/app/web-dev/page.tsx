6e4328f1d093bad37e4583d1520f988fb0fc7f79
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-11 21:19:46 +0530

    created web devolopment age

---
+++ src/app/web-dev/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const webpage: React.FC = () => {
+  return (
+    <div className='overflow-hidden' >
+      <h1 className="text-6xl text-center md:text-6xl lg:text-6xl font-bold font-karla bg-clip-text text-transparent bg-gradient-to-b from-red-100 via-red-300 to-red-500 drop-shadow-2xl shadow-red-500/30 p-6 md:p-8 lg:p-10 rounded-lg transform hover:scale-105 transition-transform duration-300 ease-in-out ">
+        Web Development
+      </h1>
+    </div>
+  );
+}
+
+export default webpage;
