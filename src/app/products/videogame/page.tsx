a546032878e1e8de107cdc348975fa7131ba4961
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-13 21:07:49 +0530

    added basic stuff to video games page

--- src/app/products/videogame/page.tsx
+++ src/app/products/videogame/page.tsx
@@ -1,12 +1,36 @@
 
 import React from 'react';
+import App from '@/Components/App';
+import { SparklesCore } from '@/Components/sparkles';
 
 const VideoGamePage: React.FC = () => {
   return (
-    <div>
-      <h1>Video Games</h1>
-      <p>This page will showcase video games.</p>
-    </div>
+    <App>
+      <div>
+        <div className="relative h-screen overflow-hidden">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={1.0}
+            maxSize={4.0}
+            particleDensity={120}
+            className="absolute inset-0"
+            particleColor="#383838"
+            speed={5}
+          />
+          <div className="flex flex-col justify-center items-center p-4">
+           <div className="max-w-screen-lg w-full">
+           <div className="flex flex-col items-center justify-center mb-6">
+         <h1 className="text-6xl text-center font-bold font-karla bg-clip-text text-transparent bg-gradient-to-b from-red-300 via-red-500 to-black drop-shadow-2xl shadow-red-500/30 p-6 rounded-lg transform hover:scale-105 transition-transform duration-300 ease-in-out mt-3">
+          VIDEO GAMES
+        </h1>
+          <p>This page will showcase video games.</p>
+        </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </App>
   );
 };
 
