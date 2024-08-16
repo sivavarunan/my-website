e73cb47261b0620442e6efaf1407905784efdc31
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-17 01:11:03 +0530

    update on book.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['images.unsplash.com', 'assets.aceternity.com'],
+  },
+};
+
+export default nextConfig;
