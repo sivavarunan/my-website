f7f3297bcdefa9bd5b2e6224f58b2e0346c0601f
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-17 01:11:18 +0530

    .

--- src/app/products/books/page.tsx
+++ src/app/products/books/page.tsx
@@ -1,13 +1,37 @@
 
 import React from 'react';
+import App from '@/Components/App';
+import { SparklesCore } from '@/Components/sparkles';
 
 const BooksPage: React.FC = () => {
   return (
+    <App>
     <div>
-      <h1>Books</h1>
-      <p>This page will showcase Books.</p>
+      <div className="relative h-screen overflow-hidden">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={1.0}
+          maxSize={4.0}
+          particleDensity={120}
+          className="absolute inset-0"
+          particleColor="#383838"
+          speed={5}
+        />
+        <div className="flex flex-col justify-center items-center p-4 mt-10">
+         <div className="max-w-screen-lg w-full">
+         <div className="flex flex-col items-center justify-center mb-6">
+       <h1 className="text-6xl text-center font-bold font-karla bg-clip-text text-transparent bg-gradient-to-b from-red-300 via-red-500 to-black drop-shadow-2xl shadow-red-500/30 p-6 rounded-lg transform hover:scale-105 transition-transform duration-300 ease-in-out mt-3">
+        BOOKS
+      </h1>
+        <p>This page will showcase Books.</p>
+      </div>
+      </div>
+      </div>
+      </div>
     </div>
-  );
+  </App>
+);
 };
 
 export default BooksPage;
--- src/app/products/videogame/page.tsx
+++ src/app/products/videogame/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import App from '@/Components/App';
 import { SparklesCore } from '@/Components/sparkles';
+import { ExpandableCardDemo } from '@/Components/ExpandableCard';
 
 const VideoGamePage: React.FC = () => {
   return (
@@ -18,17 +19,18 @@ const VideoGamePage: React.FC = () => {
             particleColor="#383838"
             speed={5}
           />
-          <div className="flex flex-col justify-center items-center p-4">
-           <div className="max-w-screen-lg w-full">
-           <div className="flex flex-col items-center justify-center mb-6">
-         <h1 className="text-6xl text-center font-bold font-karla bg-clip-text text-transparent bg-gradient-to-b from-red-300 via-red-500 to-black drop-shadow-2xl shadow-red-500/30 p-6 rounded-lg transform hover:scale-105 transition-transform duration-300 ease-in-out mt-3">
-          VIDEO GAMES
-        </h1>
-          <p>This page will showcase video games.</p>
+          <div className="flex flex-col justify-center items-center p-4 mt-10">
+            <div className="max-w-screen-lg w-full">
+              <div className="flex flex-col items-center justify-center mb-6">
+                <h1 className="text-6xl text-center font-bold font-karla bg-clip-text text-transparent bg-gradient-to-b from-red-300 via-red-500 to-black drop-shadow-2xl shadow-red-500/30 p-6 rounded-lg transform hover:scale-105 transition-transform duration-300 ease-in-out mt-3">
+                  VIDEO GAMES
+                </h1>
+                <p>This page will showcase video games i played and fell in love with.</p>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
+        <div className='z-50'> <ExpandableCardDemo /></div>
       </div>
     </App>
   );
