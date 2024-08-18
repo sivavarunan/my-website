d458f80082ff20a27f1aed5593087511e411448e
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-19 03:47:13 +0530

    still working on the styles missing after deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import App from '@/Components/App';  
-// import HomePage from '@/app/pages/HomePage';
+import HomePage from '@/app/pages/HomePage';
+import "./globals.css";
 
 const IndexPage: React.FC = () => {
   return (
     <App>
-      hi
-      {/* <HomePage /> */}
+      <HomePage />
     </App>
   );
 };
