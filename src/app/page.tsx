57390633af6583881f46fc8a87cd84527b0c6dd4
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-18 16:01:52 +0530

    debugging deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import App from '@/Components/App';  
-import HomePage from '@/app/pages/HomePage';
+// import HomePage from '@/app/pages/HomePage';
 
 const IndexPage: React.FC = () => {
   return (
     <App>
-      <HomePage />
+      hi
+      {/* <HomePage /> */}
     </App>
   );
 };
