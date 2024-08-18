ac505e2ea522648cff902e9dd72b08dbb5294de8
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-18 15:19:44 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from '@/Components/App';  
-import HomePage from './pages/HomePage';
+import HomePage from '@/app/pages/HomePage';
 
 const IndexPage: React.FC = () => {
   return (
