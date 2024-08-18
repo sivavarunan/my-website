dc6f63a1c0f0cb82c8b74d59f7bb1bc9e5a155a5
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-19 04:16:18 +0530

    ..

--- src/app/Pages/HomePage.tsx
+++ src/app/Pages/HomePage.tsx
@@ -28,7 +28,7 @@ const HomePage: React.FC = () => {
             <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 text-3xl text-center mt-4">
               Feel <span className='bg-clip-text bg-gradient-to-b from-red-300 via-red-500 to-black drop-shadow-2xl shadow-red-500/30lg rounded-lg'> Free </span> to Explore About Me..
             </p>
-            <Link href="/pages/about" passHref>
+            <Link href="/about" passHref>
               <button
                 className="inline-flex h-11 animate-shimmer items-center justify-center rounded-3xl hover:text-red-400 border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mt-6"
                 aria-label="Get started"
