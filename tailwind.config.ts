0419d186c45beb85cfb150f86a84f48e2a59e743
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-20 05:07:13 +0530

    typo in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const addVariablesForColors: PluginCreator = ({ addBase, theme }) => {
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
